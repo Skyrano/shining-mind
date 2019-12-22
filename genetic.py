bb361c8b580ef7d9443279713ed75253055d01c1
Skyrano <alistair-rameau@hotmail.fr>
2019-12-22 17:50:16 +0100

    creations des fonction genese et evolution

--- genetic.py
+++ genetic.py
@@ -1,6 +1,10 @@
 from dijkstra import *
 import random
 import math
+from PIL import Image
+import matplotlib.pyplot as plt
+import statistics
+
 
 def Deplacement(choix,x,y):
     if choix == 0:
@@ -171,12 +175,12 @@ def TestPathNew(maze,path,stX,stY,endX,endY):
     return int(Distance(x,y,endX,endY) + len(path)/2)
 
 
-def TestPathBackward(maze,path,stX,stY,endX,endY):
+def TestPathBackward(maze,path,stX,stY,endX,endY,mapping):
     matrix = maze.copy()
     x,y = stX,stY
     for i in range(len(path)):
         if len(AdjacentCrossBrut_List(matrix,x,y,1)) != 0:
-            break
+            return 0
         elif len(AdjacentCrossBrut_List(matrix,x,y,0)) == 0 and len(AdjacentCrossBrut_List(matrix,x,y,3)) == 1 and maze[x][y] != 2 and maze[x][y] != 1 and len(AdjacentCrossBrut_List(matrix,x,y,2)) == 0 and len(AdjacentCrossBrut_List(matrix,x,y,1)) == 0:
             while len(AdjacentCrossBrut_List(matrix,x,y,0)) == 0 and len(AdjacentCrossBrut_List(matrix,x,y,3)) == 1 and maze[x][y] != 2 and maze[x][y] != 1 and len(AdjacentCrossBrut_List(matrix,x,y,2)) == 0 and len(AdjacentCrossBrut_List(matrix,x,y,1)) == 0:
                 maze[x][y] = -1
@@ -188,10 +192,11 @@ def TestPathBackward(maze,path,stX,stY,endX,endY):
             if maze[nx,ny] != -1:
                 x,y = nx,ny
             matrix[x][y] = 3
+    
     return int(Distance(x,y,endX,endY) + len(path)/2)
 
 
-def InitalPop(matrix,number,length):
+def InitalPop(number,length):
     population = []
     for i in range(number):
         population.append(CreatePathCross(length))
@@ -216,17 +221,15 @@ def Selection(population_tested, taux_select):
     return path
 
 
-def Reproduction(population,taux_natalite):
-    path_coupled = []
-    nata = int(taux_natalite*len(population))
-    for i in range(nata):
+def Reproduction(population,nb_enfant):
+    for i in range(nb_enfant):
         cut = random.randint(int(0.4*len(population[0])),int(0.6*len(population[0])))
         path1 = random.choice(population)
         path2 = random.choice(population)
         while path2 == path1:
             path2 = random.choice(population)
-        path_coupled.append(path1[:cut]+path2[cut:])
-    return path_coupled
+        population.append(path1[:cut]+path2[cut:])
+    return population
 
 
 def MutationCross(population,taux_muta):
@@ -238,28 +241,56 @@ def MutationCross(population,taux_muta):
     return population
 
 
-maze, zeros = ConstructMaze(200,200)
-mapping = Dijkstra(maze,zeros)
-stX,stY = RandomCoord(maze,0)
-endX,endY = RandomCoord(maze,0)
-maze[stX][stY] = 2
-maze[endX][endY] = 1
-print(stX,stY)
-print(endX,endY)
+def InitializeMap(X,Y):
+    maze, zeros = ConstructMaze(X,Y)
+    mapping = Dijkstra(maze,zeros)
+    stX,stY = RandomCoord(maze,0)
+    endX,endY = RandomCoord(maze,0)
+    maze[stX][stY] = 2
+    maze[endX][endY] = 1
+    return maze,mapping,stX,stY,endX,endY
 
 
-MatrixPrinting(maze,"Maze.bmp")
-pop_init = InitalPop(maze,100,300)
-pop_ordered,fitness = TestPopulation(pop_init,maze,stX,stY,endX,endY)
-MatrixPrinting(maze,"Maze2.bmp")
-print(len(pop_ordered))
+def Genese(N,L):
+    return InitalPop(N,L)
+    
+    
+def Evolution(pop_init,X,Y,nG,ts,tm):
+    fit_liste = []
+    maze,mapping,stX,stY,endX,endY = InitializeMap(X,Y)
+    MatrixPrinting(maze,"Maze.bmp")
+    N = len(pop_init)
+    Ne = int(N*(1-ts))
+    
+    for i in range(nG):
+        pop_ordered,fitness = TestPopulation(pop_init,maze,stX,stY,endX,endY)
+        print(fitness)
+        if 0 in fitness == True:
+            print("Solution trouvee")
+            break 
+        fit_liste.append(statistics.mean(fitness))
+        MatrixPrinting(maze,"Maze{}.bmp".format(i))
+        
+        selection = Selection(pop_ordered,ts)
+        print("selection ",selection)
+        repro = Reproduction(selection,Ne)
+        print("repro ",repro)
+        mutation = MutationCross(repro,tm)
+        print("mutation",mutation)
+        
+        print("\n")
+        
+        input()
+     
+    plt.plot(fit_liste)
+    plt.show()
+    plt.close()
+        
+population = Genese(5,10)
 
-selection = Selection(pop_ordered,0.5)
-print(len(selection))
+Evolution(population,50,50,10,0.5,1.0)
 
-repro = Reproduction(selection,1.0)
-print(len(repro))
-
-mutation = MutationCross(repro,0.5)
-print(len(mutation))
-
+"""
+img = Image.open('Maze.bmp')
+img.show()
+"""
